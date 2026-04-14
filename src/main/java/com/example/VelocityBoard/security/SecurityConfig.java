3caf3579b7fa726388d18d3d003adfde20497bd2
CesarMoraCorrea <moracesar1232@gmail.com>
2026-04-13 19:39:32 -0500

    fix: resolver problema de threads bloqueantes con Swagger y Webjars usando UI manual

--- src/main/java/com/example/VelocityBoard/security/SecurityConfig.java
+++ src/main/java/com/example/VelocityBoard/security/SecurityConfig.java
@@ -45,7 +45,8 @@ public class SecurityConfig {
                 .securityContextRepository(securityContextRepository)
                 .authorizeExchange(exchanges -> exchanges
                         .pathMatchers(HttpMethod.OPTIONS).permitAll()
-                        .pathMatchers("/auth/**", "/index.html", "/").permitAll()
+                        .pathMatchers("/auth/**", "/index.html", "/", "/swagger.html").permitAll()
+                        .pathMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html", "/webjars/**").permitAll()
                         .anyExchange().authenticated()
                 )
                 .build();
